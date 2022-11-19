8e06ac595cbeb695a7992d3470a664058db778e5
asus i3 <tantrifadhillahasri@gmail.com>
2022-11-19 22:53:40 +0700

    Challenge8

--- config/database.js
+++ config/database.js
@@ -18,7 +18,7 @@ module.exports = {
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: DB_NAMA,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres"
